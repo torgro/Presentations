3042473ca62efb309ea0978c42ed3c2240483c89
DESKTOP-D4CMR1V\tore <tore.groneng@gmail.com>
2016-11-23 20:53:54 +0100

    Initial commit

---
+++ MTUG/23.11.2016/demo1.ps1
@@ -0,0 +1,216 @@
+﻿#region Know thy cmdlets
+
+#region Get-Content
+
+    Get-ChildItem -Path .\strings1.txt | 
+        foreach {
+            [PScustomobject]@{
+                File = $_.Name 
+                SizeKB = [int]($_.Length / 1kb)
+                }
+        }
+
+    [gc]::Collect()
+    Measure-Command -Expression {
+        $Content = Get-Content -Path .\strings1.txt -Encoding UTF8
+    }
+    
+    $Content.GetType()
+    $content.Count
+    $Content[0]
+
+
+    [gc]::Collect()
+    Measure-Command -Expression {
+        $Content = Get-Content -Raw -Path .\strings1.txt -Encoding UTF8
+    }
+
+    $Content.GetType()
+    $content.Count
+    $Content[0]
+
+
+
+    [gc]::Collect()
+    Measure-Command -Expression {
+        $Content = Get-Content -ReadCount 0 -Path .\strings1.txt -Encoding UTF8
+    }
+
+    $Content.GetType()
+    $content.Count
+    $Content[0]
+
+
+
+    [gc]::Collect()
+    Measure-Command -Expression {
+        $Content = Get-Content -Raw -Path .\strings1.txt
+    }
+
+    $Content.GetType()
+    $Content.Length
+    $Content[0..15] -join ""
+
+
+
+    Get-ChildItem -Path ".\strings?.txt" | 
+        foreach {
+            [PScustomobject]@{
+                File = $_.Name 
+                SizeKB = [int]($_.Length / 1kb)
+                }
+        }
+
+    $measure = foreach ($file in (Get-ChildItem -Path ".\strings?.txt")) 
+    {
+        [gc]::Collect()
+        $m = Measure-Command -Expression {
+            $content = [System.IO.File]::ReadAllBytes($File.fullname)
+        }
+        [pscustomobject]@{
+            File = $file.Name
+            ms = $m.Milliseconds
+            Ticks = $m.Ticks
+        }
+    } 
+
+    $Content.GetType()
+    $content[0..10]
+
+    $measure
+
+    #endregion
+
+#region Select-String
+
+    Get-Content -Path .\strings1.txt -Encoding UTF8 | Select-Object -first 10
+    
+    [gc]::Collect()
+    Measure-Command -Expression {
+        Get-ChildItem -Filter strings?.txt | Get-Content | Select-String -Pattern "Results"
+    }
+
+
+    
+    [gc]::Collect()
+    Measure-Command -Expression {
+        Get-ChildItem -Filter strings?.txt | Select-String -Pattern "Results"
+    }
+
+
+#endregion
+
+#region Where-Object
+
+    Get-ChildItem -Path .\userlist.csv | 
+        foreach {
+            [PScustomobject]@{
+                File = $_.Name 
+                SizeKB = [int]($_.Length / 1kb)
+                }
+        }
+
+    $userList = Import-Csv -Path .\userlist.csv -Delimiter "," -Encoding UTF8
+
+    $userList.Count
+
+    $userList | Select-Object -first 10
+
+    
+
+    [gc]::Collect()
+    Measure-Command -Expression {
+        $userList | Where-Object id -eq 1813981965
+    }
+
+
+
+    [gc]::Collect()
+    Measure-Command -Expression {
+        $userList.Where({ $_.id -eq 1813981965 })
+    }
+
+
+
+
+    [gc]::Collect()
+    Measure-Command -Expression {
+        for ($i = 1; $i -lt $userList.Count; $i++)
+        { 
+            if ($userList[$i].id -eq 1813981965)
+            {
+                $user
+            }
+        }
+    }
+
+
+
+    [gc]::Collect()
+    Measure-Command -Expression {
+        foreach ($user in $userList)
+        {
+            if ($user.id -eq 1813981965)
+            {
+                $user
+            }
+        }
+    }
+
+
+
+    $userHashList = @{}
+
+    foreach ($user in $userList)
+    {
+        $userHashList.Add($user.id,$user)
+    }
+
+
+    [gc]::Collect()
+    Measure-Command -Expression {
+        $userHashList["1813981965"]
+    }
+
+    
+
+
+#endregion
+
+#region Foreach-Object
+
+    [gc]::Collect()
+    Measure-Command -Expression {
+        $userList | ForEach-Object {
+            if($_.id -eq 1813981965)
+            {
+                $_
+            }
+       }
+    }
+
+    [gc]::Collect()
+    Measure-Command -Expression {
+        $userList.ForEach({
+            if($_.id -eq 1813981965)
+            {
+                $_
+            }
+       })
+    }
+
+    [gc]::Collect()
+    Measure-Command -Expression {
+        foreach ($user in $userList)
+        {
+            if ($user.id -eq 1813981965)
+            {
+                $user
+            }
+        }
+    }
+
+
+#endregion
+
+#endregion
---
+++ MTUG/23.11.2016/demo2.ps1
@@ -0,0 +1,151 @@
+﻿#region Collections
+
+#region Array
+
+    [gc]::Collect()
+    $array = @()
+    Measure-Command -Expression {
+        foreach ($int in (0..10000))
+        {
+            $array += Get-Random
+        }
+    }
+
+    $array.Count
+   
+
+
+    [gc]::Collect()
+    $arrayList = New-Object -TypeName System.Collections.ArrayList
+    Measure-Command -Expression {
+        foreach ($int in (0..10000))
+        {
+            $null = $arrayList.Add((Get-Random))
+        }
+    }
+
+    $arrayList.GetType()
+
+    $arrayList.Count
+
+
+
+    [gc]::Collect()
+    Measure-Command -Expression {
+        $array = $arrayList.ToArray()
+    }
+
+    $array.GetType()
+
+    $array.Count
+
+#endregion
+
+#region HashTables
+
+    Get-ChildItem -Path .\upn.csv | 
+        foreach {
+            [PScustomobject]@{
+                File = $_.Name 
+                SizeKB = [int]($_.Length / 1kb)
+                }
+        }
+
+    $userList.Count
+
+    $upnList = Import-Csv -Path .\upn.csv -Delimiter "," -Encoding UTF8
+    $upnList.Count
+
+    $upnList | Select-Object -first 10
+
+
+    [gc]::Collect()
+    $upnHashLookup = @{}
+    Measure-Command -Expression {
+        foreach ($upn in $upnList)
+        {
+            $upnHashLookup.Add($upn.id,$upn)
+        }
+    }
+
+
+    
+    $newUserList = New-Object -TypeName System.Collections.ArrayList
+
+    [gc]::Collect()
+    Measure-Command -Expression {
+        foreach ($User in $userList)
+        {
+            $upn = $upnHashLookup[$user.id].UserPrincipalName        
+            $newUser = [pscustomobject]@{
+                id = $user.id
+                UserName = $user.UserName
+                Location = $user.Location
+                Description = $user.Description
+                Upn = $upn
+            }
+            $null = $newUserList.Add($newUser)
+        
+            #Add-Member -InputObject $user -MemberType NoteProperty -Name Upn -Value $upn -PassThru
+        }
+    }
+
+#endregion
+
+#region Select-Object Unique
+    
+    # Took 00:13:23
+    [gc]::Collect()
+    Measure-Command -Expression {
+        $userList.id | Select-Object -Unique
+    }
+    
+
+    function Select-UniqueObject
+    {
+    [cmdletbinding()]
+    Param(
+        [Parameter(ValueFromPipeline)]
+        [array]$Inputobject
+    )
+    Begin
+    {
+        $hashtable = @{}
+    }
+
+    Process
+    {
+        foreach($element in $Inputobject)
+        {
+            if (-not $hashtable.ContainsKey($element))
+            {
+                $hashtable.Add($element,[string]::Empty)
+            }
+        }
+    }
+
+    End
+    {
+        $hashtable.Keys
+    }
+
+    }
+
+
+    $id = $userList[1].id
+
+    $userList[1].id = $userList[0].id
+
+    $userList | Select-Object -First 2
+
+    $userList[1].id = $id
+
+
+    [gc]::Collect()
+    Measure-Command -Expression {
+        (Select-UniqueObject -Inputobject $userList.id).count
+    }
+
+#endregion
+
+#endregion
